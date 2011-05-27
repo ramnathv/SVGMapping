0636a81e9ac3f9a8ad4d05d1953a5364d76dc73c
jcaude <jcaude@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-27 12:44:50 +0000

    Website
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/svgmapping@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ www/SVG_Mapping/Source-G1_files/Source-G1.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.3 local-build-20110527
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Source-G1_files/Source-G1Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
