a25ab79a3666dfbd884c86602b53ca55cdde8d79
jcaude <jcaude@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-25 14:16:14 +0000

    [WIP] Working on updating the tutorial section (#1544)
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/svgmapping@57 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ www/SVG_Mapping/Tutorials_files/Tutorials.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20110825
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Tutorials_files/TutorialsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
