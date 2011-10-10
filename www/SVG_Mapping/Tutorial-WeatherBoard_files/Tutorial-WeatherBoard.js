e4ccb1c9f8da051d3a0593c0e1ddfb1984c103f6
jcaude <jcaude@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-10-10 08:19:47 +0000

    Fix. Inkscape Gauges OS/X Package link
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/svgmapping@95 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/SVG_Mapping/Tutorial-WeatherBoard_files/Tutorial-WeatherBoard.js
+++ www/SVG_Mapping/Tutorial-WeatherBoard_files/Tutorial-WeatherBoard.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20111007
+// Created by iWeb 3.0.4 local-build-20111010
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000})});registry.applyEffects();}
